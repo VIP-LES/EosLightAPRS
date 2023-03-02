0084d34648cfdcb02e9cc8ff422ba9b32aa44883
Thomas Holder <tholder7@gatech.edu>
2023-03-01 19:59:44 -0500

    Fix symbol

--- LightAPRS-hab/example-aprs-config.h
+++ LightAPRS-hab/example-aprs-config.h
@@ -5,7 +5,7 @@
 // MUST CHANGE THIS
 
 #define CALLNUMBER 11 //SSID http://www.aprs.org/aprs11/SSIDs.txt
-#define SYMBOL '0' // '/O' for balloon, '/>' for car, for more info : http://www.aprs.org/symbols/symbols-new.txt
+#define SYMBOL 'O' // '/O' for balloon, '/>' for car, for more info : http://www.aprs.org/symbols/symbols-new.txt
 #define ALT_SYMBOL false //false = '/' , true = '\'
 
 #define FREQUENCY "144.3900" //default frequency. 144.3900 for US, 144.8000 for Europe
